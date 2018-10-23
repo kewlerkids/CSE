09db3f2ab6f6f876c6d507bf922c0e7cd1e724d0
kewlerkids <42384002+kewlerkids@users.noreply.github.com>
2018-10-23 08:33:52 -0700

    Initial commit

---
+++ notes/Basic Python Notes.py
@@ -0,0 +1,19 @@
+print("Hello World")
+print()
+# This is a comment. I can write whatever I want
+# Here and it wont do anything about it.
+# It has no effect on the code.
+print()   # This adds extra spaces on the terminal
+print("This will print here. Notice the spacing.")
+
+a = 4
+b = 3
+print(a+b)
+print(a*5)
+print(5-3)
+print(6/5)
+
+print("Figure this out")
+print(6//5)
+print(12//3)
+print(9//4)
