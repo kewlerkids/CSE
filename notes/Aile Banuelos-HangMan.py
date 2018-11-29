6280ebd04148eee910c97259dcae6492a3ee50aa
kewlerkids <42384002+kewlerkids@users.noreply.github.com>
2018-11-29 08:32:00 -0800

    starting hangman

--- notes/Aile Banuelos-HangMan.py
+++ notes/Aile Banuelos-HangMan.py
@@ -1,4 +1,4 @@
+ice_cream_flavours = ["Vanilla", "Chocolate", "Pistachio", "Strawberry",
+                      "Rocky Road", "Mint Chocolate Chip", "Coffee", "Cookies and Creme"]
 
-ice_cream_flavours = ["Vanilla", "Chocolate", "Pistachio", "Strawberry", "Rocky Road", "Mint Chocolate Chip","Coffee"]
-ice_cream_flavours[2] = "Cookies and Creme"
 
