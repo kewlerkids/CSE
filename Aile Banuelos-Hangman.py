02192e3db27be6bf98991cb7d7afea9e66994393
kewlerkids <42384002+kewlerkids@users.noreply.github.com>
2018-11-30 09:39:39 -0800

    fit it

---
+++ Aile Banuelos-Hangman.py
@@ -0,0 +1,5 @@
+ ice_cream_flavours = ["Vanilla", "Chocolate", "Pistachio", "Strawberry",
+                      "Rocky Road", "Mint Chocolate Chip", "Coffee", "Cookies and Creme"]
+
+random.choice("Vanilla", "Chocolate", "Pistachio", "Strawberry",
+                      "Rocky Road", "Mint Chocolate Chip", "Coffee", "Cookies and Creme")
--- notes/Aile Banuelos-HangMan.py
+++ notes/Aile Banuelos-HangMan.py
@@ -1,4 +1,6 @@
 ice_cream_flavours = ["Vanilla", "Chocolate", "Pistachio", "Strawberry",
                       "Rocky Road", "Mint Chocolate Chip", "Coffee", "Cookies and Creme"]
 
+random.choice("Vanilla", "Chocolate", "Pistachio", "Strawberry",
+                      "Rocky Road", "Mint Chocolate Chip", "Coffee", "Cookies and Creme")
 
