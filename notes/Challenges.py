0e6a57827ddabebf625cd52c77b6d49740e3f979
kewlerkids <42384002+kewlerkids@users.noreply.github.com>
2018-12-05 07:55:27 -0800

    challenges

---
+++ notes/Challenges.py
@@ -0,0 +1,35 @@
+def challenge1(name1, name2):
+    print("%s, %s" % (name2, name1))
+
+
+challenge1("Aile", "Banuelos")
+
+
+def challenge3(base, height):
+    challenge3("10", "5")
+
+    print(base*height/2)
+
+
+def challenge4(number):
+    if number == 0:
+        return "This number is zero"
+    if number > 0:
+        return "This number is positive"
+    if number < 0:
+        return "This number is negative"
+
+    number(input("Random number? "))
+
+
+def challenge7(number):
+    print(number+number*number+number**3)
+
+
+challenge7(2)
+
+
+def challenge8(number):
+    if number 150 =< >=2000:
+        return "It is between 150 and 2000"
+
