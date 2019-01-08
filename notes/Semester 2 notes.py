abb529dd668b955367bcfd4b3f71da61225bb69a
kewlerkids <eo87@wetigers.org>
2019-01-08 08:44:05 -0800

    semester 2 notes

---
+++ notes/Semester 2 notes.py
@@ -0,0 +1,20 @@
+print("Hello world!")
+# This is how to print things
+cars = 5
+driving = True
+# cars = 5 → "5"
+
+print("I have %s cars" % cars)
+print("I have " + str(cars) + " cars")
+
+age = input("How old are you?")
+print(age + "? You're so young!")
+
+# Make a list of at least 5 colors
+colors = ["cyan", "pink", "magenta", "green", "white"]
+colors.append("red")
+
+print(colors)
+
+colors.pop(0)
+print(colors)
