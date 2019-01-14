863cee881867f033276c46c7328c9dac57b7137e
kewlerkids <eo87@wetigers.org>
2019-01-14 08:43:35 -0800

    more hangman

--- notes/Aile Banuelos-HangMan.py
+++ notes/Aile Banuelos-HangMan.py
@@ -5,26 +5,30 @@ word_bank = ["Vanilla Bean", "Chocolate", "Pistachio", "Strawberry", "Rocky Road
              "Espresso Chocolate Cookie Crumble", "Sea Salt Caramel Truffle"]
 
 guess_word = random.choice(word_bank)
-print(guess_word)
 
-total_turns = 8
-print("You have %s turns to start with" % total_turns)
+total_lives = 8
+print("You have %s turns to start with" % total_lives)
 
-while total_turns > 0:
+
+while total_lives > 0:
     players_guess = input("Letter?")
 
     string1 = guess_word
     list1 = list(string1)
-    print(list1)
+    print("".join(list1))
+
+    guessed_letters = []
 
     for character in list1:
-        if character == "u":
+        if character == "":
             current_index = list1.index(character)
             list1.pop(current_index)
             list1.insert(current_index, "*")
 
-        else character:
+        # else character == 0:
             current_index = list1.index(character)
-            total_guess -= 1
+            total_lives -= 1
 
-    print("You have %s turns left"% total_turns)
+    print("You have %s lives left"% total_lives)
+    # You have your word then it becomes a list of letters
+    # and you have to see if the player guesses something in the list
