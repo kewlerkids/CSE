724a5914870c3486fdfd34cc051e9a404e7e2123
kewlerkids <eo87@wetigers.org>
2019-01-15 08:39:36 -0800

    hangman

--- notes/Aile Banuelos-HangMan.py
+++ notes/Aile Banuelos-HangMan.py
@@ -1,5 +1,5 @@
 import random
-
+import string
 word_bank = ["Vanilla Bean", "Chocolate", "Pistachio", "Strawberry", "Rocky Road!", "Mint Chocolate Chip",
             "Coffee", "Cookies and Creme", "Do you like Ice cream?", "What is butter pecan?",
              "Espresso Chocolate Cookie Crumble", "Sea Salt Caramel Truffle"]
@@ -8,7 +8,7 @@ guess_word = random.choice(word_bank)
 
 total_lives = 8
 print("You have %s turns to start with" % total_lives)
-
+print(list(string.ascii_letters))
 
 while total_lives > 0:
     players_guess = input("Letter?")
@@ -16,11 +16,15 @@ while total_lives > 0:
     string1 = guess_word
     list1 = list(string1)
     print("".join(list1))
-
-    guessed_letters = []
+    correct_letters = (list(string1))
+    
+    blanks = '_' * len(guess_word)
+    for i in range(len(guess_word)):
+        if guess_word[i] in correct_letters:
+            blanks = blanks[:i] + guess_word[i] + blanks[i + 1:]
 
     for character in list1:
-        if character == "":
+        if character == ("%s" % players_guess):
             current_index = list1.index(character)
             list1.pop(current_index)
             list1.insert(current_index, "*")
@@ -29,6 +33,5 @@ while total_lives > 0:
             current_index = list1.index(character)
             total_lives -= 1
 
-    print("You have %s lives left"% total_lives)
-    # You have your word then it becomes a list of letters
-    # and you have to see if the player guesses something in the list
+    print("You have %s lives left" % total_lives)
+
