69e6395dba94a67475b6530f74e68e9953e147be
kewlerkids <eo87@wetigers.org>
2019-01-22 08:33:46 -0800

    hangman

--- notes/Aile Banuelos-HangMan.py
+++ notes/Aile Banuelos-HangMan.py
@@ -1,5 +1,5 @@
 import random
-# import string
+import string
 word_bank = ["Vanilla Bean", "Chocolate", "Pistachio", "Strawberry", "Rocky Road!", "Mint Chocolate Chip",
             "Coffee", "Cookies and Creme", "Do you like Ice cream?", "What is butter pecan?",
              "Espresso Chocolate Cookie Crumble", "Sea Salt Caramel Truffle"]
@@ -8,7 +8,8 @@ guess_word = random.choice(word_bank)
 
 total_lives = 8
 print("You have %s turns to start with" % total_lives)
-# print(list(string.ascii_letters))
+print(list(string.ascii_letters))
+guessed_letters = []
 
 while total_lives > 0:
     players_guess = input("Letter?")
@@ -24,14 +25,7 @@ while total_lives > 0:
         if guess_word[i] in correct_letters:
             blanks = blanks[:i] + guess_word[i] + blanks[i + 1:]
 
-    for character in list1:
-        if character == ("%s" % players_guess):
-            current_index = list1.index(character)
-            list1.pop(current_index)
-            list1.insert(current_index, "*")
-
         else:
-            current_index = list1.index(character)
             total_lives -= 1
 
     print("You have %s lives left" % total_lives)
