9bfb33bd16539e6688e0a5047bc7bfb05e0ca7ed
kewlerkids <eo87@wetigers.org>
2019-01-31 08:43:49 -0800

    csgo map baby

--- notes/Aile Banuelos- Dictionary World map.py
+++ notes/Aile Banuelos- Dictionary World map.py
@@ -16,3 +16,24 @@ world_map = {
         }
     }
 }
+
+# Other Variables
+current_node = world_map['R19A']
+directions = ["NORTH", "SOUTH", "EAST", "WEST", "UP", "DOWN"]
+playing = True
+
+# Controller
+while playing:
+    print(current_node["NAME"])
+    print(current_node["DESCRIPTION"])
+    command = input(">_")
+    if command.lower() in ['q', 'quit', 'exit']:
+        playing = False
+    elif command.upper()in directions:
+        try:
+            room_name = current_node['PATHS'][command.upper()]
+            current_node = world_map[room_name]
+        except KeyError:
+            print("I can't go that way.")
+    else:
+        print("Command Not Recognized")
---
+++ notes/Aile Banuelos- World Map.py
@@ -0,0 +1,18 @@
+world_map = {
+    'R1A': {
+        'NAME': "Lower Mid",
+        'DESCRIPTION': "You are at a narrow path and all you can do is go forward.",
+        'PATHS': {
+            'NORTH': "R1B"
+        }
+
+    },
+    'R1B': {
+        'NAME': "Mid",
+        'DESCRIPTION': "You are at a narrow path and you have directions every way."
+    },
+    'R22': {
+        'NAME': "Bootyful Wall",
+        'DESCRIPTION': "You can't go that way silly it's a wall."
+    }
+}
