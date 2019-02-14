c83855738925a069676037eea057336f864ce53d
kewlerkids <eo87@wetigers.org>
2019-02-14 08:44:20 -0800

    meh boook

--- notes/MyObject.py
+++ notes/MyObject.py
@@ -1,1 +1,23 @@
+class Book (object):
+    def __init__(self, cover, pages, titles, colors):
+        self.cover = cover
+        self.pages = pages
+        self.size = 7
+        self.title = titles
+        self.color = colors
 
+    def open(self):
+        if self.pages:
+            Book.pages = ()
+
+    def rip(self):
+        self.pages
+
+
+harry_potter = Book(hard, 636, 'Harry Potter', black)
+percy_jackson = Book(hard,)
+the_hobbit = Book()
+hunger_games = Book()
+life_of_pi = Book()
+cat_in_the_hat = Book()
+the_chronicles_of_narnia = Book(paper,)
