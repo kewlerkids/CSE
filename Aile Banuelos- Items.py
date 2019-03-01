4d7e37ab17a206d7266af058d71935348c347bc4
kewlerkids <eo87@wetigers.org>
2019-03-01 09:45:15 -0800

    yes

---
+++ Aile Banuelos- Items.py
@@ -0,0 +1,17 @@
+# Engine, hull, key, wrench, wheel, baseball bat, book, apple, paint, gas.
+class Item(object):
+    def __init__(self):
+
+
+class Vehicle(object):
+      def __init__(self, name, engine):
+           self.name = name
+           self.engine_type = engine
+
+        class Car(Vehicle):
+            def __init__(self, name, engine_type, body_type):
+                super(Car, self).__init__(name, engine_type)
+                self.body_type = body_type
+                self.steering_wheel = True
+                self.engine_status = False  # because the engine is off
+                self.fuel = 100
