0460236e79555dcc06553688912789b998b4cda2
kewlerkids <eo87@wetigers.org>
2019-03-08 08:43:20 -0800

    stuffs

--- Aile Banuelos- Items.py
+++ Aile Banuelos- Items.py
@@ -9,7 +9,7 @@ class Items(object):
 
 class Fruit(Items):
     def __init__(self, location, name, color, type_of):
-        super(Items, self).__init__(location, name, color, type_of)
+        super(Fruit, self).__init__(location, name)
         self.color = color
         self.type = type_of
 
@@ -31,13 +31,29 @@ class BlackBerry(Fruit):
 
 class Tools(Items):
     def __init__(self, location, name, type_of):
-        super(Items, self).__init__(location, name, type_of)
+        super(Tools, self).__init__(location, name)
         self.type = type_of
 
 
 class Hammer(Tools):
     def __init__(self):
         super(Hammer, self).__init__("CUB", "Hammer", "Thick")
+
+
+class Screw(Tools):
+    def __init__(self):
+        super(Screw, self).__init__("MOT", "Screw Driver", "Small")
+
+
+class Utensil(Items):
+    def __init__(self, location, name, type_of):
+        super(Utensil, self).__init__(location, name)
+        self.type = type_of
+
+
+class Spoon(Utensil):
+    def __init__(self):
+        super(Spoon, self).__init__("KIT", "Spoon", "Rusty")
 
 
 class Vehicle(object):
@@ -50,9 +66,10 @@ class Car(Vehicle):
     def __init__(self, name, engine_type, body_type):
         super(Car, self).__init__(name, engine_type)
         self.body_type = body_type
-        self.steering_wheel = True
+        self.hull = False
+        self.steering_wheel = False
         self.engine_status = False  # because the engine is off
-        self.fuel = 100
+        self.fuel = 0
 
     def start_engine(self):
         self.engine_status = True
@@ -73,3 +90,7 @@ class Car(Vehicle):
     def turn_off(self):
         self.engine_status = False
         print("You turn the engine off.")
+
+
+berry = BlackBerry()
+print(berry.name)
