02f7d4e6b369712ee33975b2d8ee86643374b3e5
kewlerkids <eo87@wetigers.org>
2019-04-05 09:41:08 -0700

    yes

--- notes/Aile Banuelos-World Map OOP.py
+++ notes/Aile Banuelos-World Map OOP.py
@@ -217,6 +217,17 @@ class Character(object):
 # c2 = Character("Orc2", 100, sword2, None)
 # c1.attack(c2)
 
+
+wheel1 = Wheel()
+wrench1 = Wrench()
+key1 = Key()
+hull1 = Hull()
+engine1 = Engine()
+gas1 = Gas()
+hammer1 = Hammer()
+paint1 = Paint()
+screw1 = Screw()
+
 # ====================================================================================================================
 # Rooms
 # ====================================================================================================================
@@ -228,12 +239,12 @@ R1A = Room('Lower Mid', "R1B", None, None, None,
            None, None, [Car("Vroom vroom", "gas")])
 R1B = Room('Mid', "R1C", "R34", "R1A", "REST", "You are at a narrow path still and you have directions every way.",
            None, None)
-REST = Room('Bench', None, "R1B", None, None, "This is just an old bench...and a Key on the floor", None, None, [Key()])
+REST = Room('Bench', None, "R1B", None, None, "This is just an old bench...and a Key on the floor", None, None, [key1])
 R34 = Room('HayStack', None, "ALL", None, "R1B", "You are near a balcony and you can go West or East.", "RBIDY", None)
 ALL = Room('Alley', "STR", None, "R34", None, "There is a door North of you and a BaseballBat on the floor.",
            None, None, [BaseballBat()])
 STR = Room('Stairs', None, None, "ALL", None, "You can go Up some stairs and on the side there is a cars Hull.",
-           "EAPT", None, [Hull()])
+           "EAPT", None, [hull1])
 RBIDY = Room('Balcony', "RWN", None, None, None,
              "You're on a balcony you can jump Down into the haystacks or go North through the window.", None, "R34")
 RWN = Room('West of Bedroom', "WAPT", "REN", "RBIDY", None, "You are at the west end of a bedroom.", None, None)
@@ -242,8 +253,8 @@ REN = Room('East of Bedroom', None, None, None, "RWN",
            None, None)
 WAPT = Room('West Apartments', None, "EAPT", "RWN", None,
             "There are so stairs going Down and a hallway East plus a door behind you South.", None, "BLR")
-EAPT = Room('East Apartments', "HALL", None, None, "WAPT", "You are next to a long hall North and stairs Down.", None,
-            "STR")
+EAPT = Room('East Apartments', "HALL", None, None, "WAPT", "You are next to a long hall North and South, stairs Down.",
+            None, "STR")
 HALL = Room('Long Hall', "DRO", None, "EAPT", None,
             "You are in the middle of a narrow and dark hall, but there is a Baseball on the floor.",
             None, None, [Baseball()])
@@ -253,7 +264,7 @@ BALL = Room('Second Balcony', None, None, "DRO", None,
             "You are at a very high place but you see a building if you go Down.", None, "VROOM")
 BLR = Room('Boiler Room', "RBLD", None, None, None,
            "There's a boiler behind me and a door in front, North, plus some stairs Up, and a Wrench.", "WAPT", None,
-           [Wrench()])
+           [wrench1])
 R1C = Room('Upper Mid', None, "RBLD", "R1B", "RCLA",
            "There is a wall North and you have paths every other way.", None, None)
 RCLA = Room('Close', None, "R1C", None, "LARCH", "It's a small corner you can go straight or back.", None, None)
@@ -263,7 +274,7 @@ VROOM = Room('Hay Truck', None, None, None, None,
              "You are on a soft truck of hay you can jump Up onto a balcony or get Down...", "BALL", "TS")
 TS = Room('Truck Side', "FGR", None, "PAT", "BOOT",
           "You are next to a wagon and a building, you can try jumping Up on the wagon, or take the Wheel.",
-          "VROOM", None, [Wheel()])
+          "VROOM", None, [wheel1])
 PAT = Room('Patio', "TS", None, None, "RBLD",
            "You are underneath a patio that has an Apple, Banana, and a BlackBerry on the floor...",
            None, None, [Apple(), Banana(), BlackBerry()])
@@ -271,17 +282,17 @@ LARCH = Room('Lower Arch Side', "UARCH", "RCLA", "CUB", None,
              "You can go into a little corner south or continue.", None, None)
 CUB = Room('Cubby', "LARCH", None, None, None, "You are in a small corner with a huge wooden box that's open..."
                                                "You look at it...there is also an Engine inside this box. Leave North.",
-           None, None, [Engine()])
+           None, None, [engine1])
 UARCH = Room('Upper Arch Side', "LDR", "MCRH", "LARCH", None,
              "You see a door in front of you and a building to your east.", None, None)
 LDR = Room('Library', None, None, "UARCH", "KIT",
            "It smells like fresh paper and there are shelves of Books and a small hall West.", None, None, [Books()])
 KIT = Room('Kitchen', None, "LDR", None, None, "There is a kitchen here with pots pans, empty cabinets, and a Gas can.",
-           None, None, [Gas()])
+           None, None, [gas1])
 MCRH = Room('Moto Near Arch', "MOT", "OFF", None, "UARCH",
             "You are near a small corner to your north and a building east.", None, None)
 MOT = Room('Moto', None, None, "MCRH", None,
-           "You are in a small corner and there is a plain cardboard box with a Hammer.", None, None, [Hammer()])
+           "You are in a small corner and there is a plain cardboard box with a Hammer.", None, None, [hammer1])
 OFF = Room('Off A Site', None, "FGR", "AST", "MCRH", "There is a wall North but directions every way.", None, None)
 FGR = Room('Front of Graveyard', "GRR", "PT", "TS", "OFF",
            "There is a graveyard north a slope east and a building west.", None, None)
@@ -293,14 +304,14 @@ PT = Room('Pit', "NPT", None, None, "FGR", "You are near an enclosed area and a 
 BOOT = Room('Boost', "DEF", None, None, "PILL",
             "You are on a wooden box that seems very fragile and are under a patio.", None, None)
 BACK = Room('Back A site', "AST", "PILL", None, None,
-            "You are in a corner at the back of the site with a Paint bucket ont the floor.", None, None, [Paint()])
+            "You are in a corner at the back of the site with a Paint bucket ont the floor.", None, None, [paint1])
 DEF = Room('Default box', "FGR", "TS", "BOOT", "AST", "You are next to large boxes you can go multiple ways.",
            None, None)
 NPT = Room('North of Pit', None, None, "PT", None,
            "You are in an enclosed area that looks very hidden with Spoons all over the floor.", None, None, [Spoon()])
 PILL = Room('Pillar', None, "BOOT", None, "BACK",
             "You are hidden behind a pillar, with a Screw on the floor, you can go East or West.", None, None,
-            [Screw()])
+            [screw1])
 
 
 # ====================================================================================================================
@@ -333,7 +344,7 @@ player = Player(R1A)
 # ====================================================================================================================
 # Controller
 # ====================================================================================================================
-items_needed = ['Wheel', 'Hull', 'Engine', 'Key', 'Wrench', 'Hammer', 'Screw', 'Paint', 'Gas']
+items_needed = [wheel1, hull1, engine1, key1, wrench1, hammer1, screw1, paint1, gas1]
 playing = True
 directions = ['north', 'south', 'east', 'west', 'up', 'down']
 
@@ -341,6 +352,9 @@ while playing:
     CarRepair = False
     print(player.current_location.name)
     print(player.current_location.description)
+
+    print(items_needed)
+    print(player.inventory)
     command = input(">_")
     if command.lower() in ['q', 'quit', 'exit']:
         playing = False
@@ -367,7 +381,7 @@ while playing:
             if item.name == item_name:
                 found_item = item
         if found_item is not None:
-            player.inventory.append(found_item.name)
+            player.inventory.append(found_item)
             player.current_location.items.remove(found_item)
             print("")
             print("You found a(n) %s" % found_item.name)
@@ -391,11 +405,8 @@ while playing:
 
     if "inventory" in command:
         print("")
-        print(player.inventory[:3])
-        print(player.inventory[3:5])
-        print(player.inventory[5:8])
-        print(player.inventory[8:11])
-        print(player.inventory[11:14])
+        print(player.inventory[:6])
+        print(player.inventory[6:])
         print("")
 
     if "list" in command:
