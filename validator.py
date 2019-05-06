0a944217adb1706c505c71c7a0e1cd59d732044a
kewlerkids <42384002+kewlerkids@users.noreply.github.com>
2019-05-06 08:44:46 -0700

    save

--- validator.py
+++ validator.py
@@ -4,8 +4,8 @@ import csv
 def validate(num: str):
     if len(num) == 16:
         list_form = list(num)
-        list_form.pop(15)
-        print(list_form)
+        last_num = list_form.pop(15)
+        print(last_num)
         reversed_form = reverse(list_form)
 
         for index in range(len(reversed_form)):
@@ -22,11 +22,8 @@ def validate(num: str):
         total = sum(reversed_form) % 10
         print(total)
 
-        if total == list_form(15):
-            return True
-
-        else:
-            return False
+        if total == last_num:
+            print("Valid")
 
 
 def reverse(num: list):
@@ -35,13 +32,13 @@ def reverse(num: list):
 
 print(validate("4556737586899855"))
 
-# with open("Book1.csv", 'r') as old_csv:
-#     with open("MyNewFile.csv", 'w', newline='') as new_csv:
-#         reader = csv.reader(old_csv)
-#         writer = csv.writer(new_csv)
-#         print("Processing...")
-#         for row in reader:
-#             old_number = row[0]  # string
-#             if validate(old_number):
-#                 writer.writerow(row)
-#         print("OK")
+with open("Book1.csv", 'r') as old_csv:
+    with open("MyNewFile.csv", 'w', newline='') as new_csv:
+        reader = csv.reader(old_csv)
+        writer = csv.writer(new_csv)
+        print("Processing...")
+        for row in reader:
+            old_number = row[0]  # string
+            if validate(old_number):
+                writer.writerow(row)
+        print("OK")
